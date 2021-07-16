2000b6008c01fb244ed6f674f6366c003ecbb20e
emirac <emilija.raciute@gmail.com>
2021-07-16 19:56:58 +0300

    Added images switcher

--- lib/providers/projects_provider.dart
+++ lib/providers/projects_provider.dart
@@ -4,6 +4,7 @@ import 'package:resume/models/project.dart';
 class ProjectsProvider extends ChangeNotifier {
   List<Project> projects = [];
   Project? selectedProject;
+  String? selectedImage;
 
   ProjectsProvider() {
     setInitialData();
@@ -16,7 +17,8 @@ class ProjectsProvider extends ChangeNotifier {
           description: 'A personal website that ',
           images: [
             'images/test1.jpeg',
-            'images/test1.jpeg',
+            'images/test2.jpeg',
+            'images/test3.jpeg',
           ],
           codeURL: 'https://github.com/emirac/resume',
           demoURL: 'https://emirac.github.io',
@@ -55,11 +57,45 @@ class ProjectsProvider extends ChangeNotifier {
       ),
     );
     this.setSelectedProject(0);
+    this.setSelectedImage(Direction.Forward);
   }
 
   void setSelectedProject(int index) {
     var selected = this.projects.elementAt(index);
     this.selectedProject = selected;
+    this.selectedImage = null;
+    this.setSelectedImage(Direction.Forward);
     this.notifyListeners();
   }
+
+  void setSelectedImage(Direction direction) {
+    if (this.selectedProject == null ||
+        this.selectedProject!.images.length == 0) {
+      return;
+    }
+    if (this.selectedImage == null) {
+      this.selectedImage = this.selectedProject!.images[0];
+    } else {
+      int index = this.selectedProject!.images.indexOf(this.selectedImage!);
+      switch (direction) {
+        case Direction.Back:
+          index =
+              index == 0 ? this.selectedProject!.images.length - 1 : index - 1;
+          break;
+        case Direction.Forward:
+          index =
+              index == this.selectedProject!.images.length - 1 ? 0 : index + 1;
+          break;
+        default:
+          break;
+      }
+      this.selectedImage = this.selectedProject!.images.elementAt(index);
+    }
+    this.notifyListeners();
+  }
+}
+
+enum Direction {
+  Back,
+  Forward,
 }
--- lib/widgtes/projects/project_card_image.dart
+++ lib/widgtes/projects/project_card_image.dart
@@ -1,7 +1,6 @@
-import 'package:carousel_slider/carousel_options.dart';
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hovering/hovering.dart';
 import 'package:provider/provider.dart';
 import 'package:resume/providers/projects_provider.dart';
 
@@ -14,32 +13,82 @@ class ProjectCardImage extends StatelessWidget {
         decoration: BoxDecoration(
           border: Border.all(color: Colors.grey.shade300),
         ),
-        child: Center(
-          child: Consumer<ProjectsProvider>(
-            builder: (_, ProjectsProvider projects, __) => CarouselSlider(
-              items: [
-                ...projects.selectedProject!.images
-                    .map((d) => Image.asset(
-                          d,
-                          fit: BoxFit.cover,
-                        ))
-                    .toList()
-              ],
-              options: CarouselOptions(
-                // height: 85,
-                // aspectRatio: 2 / 3,
-                viewportFraction: 1,
-                initialPage: 0,
-                enableInfiniteScroll: false,
-                reverse: false,
-                autoPlay: false,
-                enlargeCenterPage: false,
-                // onPageChanged: (int index, CarouselPageChangedReason reason) =>
-                //     projects.setSelectedProject(index),
-                scrollDirection: Axis.horizontal,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Consumer<ProjectsProvider>(
+              builder: (_, ProjectsProvider projects, __) => Expanded(
+                child: HoverWidget(
+                  onHover: (event) => {},
+                  child: Stack(
+                    fit: StackFit.expand,
+                    children: [
+                      Image.asset(
+                        projects.selectedImage!,
+                        fit: BoxFit.fitHeight,
+                      ),
+                    ],
+                  ),
+                  hoverChild: Stack(
+                    fit: StackFit.expand,
+                    children: [
+                      Image.asset(
+                        projects.selectedImage!,
+                        fit: BoxFit.fitHeight,
+                      ),
+                      Positioned(
+                        child: InkWell(
+                          mouseCursor: SystemMouseCursors.click,
+                          onTap: () =>
+                              projects.setSelectedImage(Direction.Back),
+                          child: Icon(
+                            Icons.arrow_back,
+                            color: Colors.white,
+                          ),
+                        ),
+                        top: 350 / 2,
+                        left: 8,
+                      ),
+                      Positioned(
+                        child: InkWell(
+                          mouseCursor: SystemMouseCursors.click,
+                          onTap: () =>
+                              projects.setSelectedImage(Direction.Forward),
+                          child: Icon(
+                            Icons.arrow_forward,
+                            color: Colors.white,
+                          ),
+                        ),
+                        top: 350 / 2,
+                        right: 8,
+                      ),
+                    ],
+                  ),
+                ),
+                // items: [
+                //   ...projects.selectedProject!.images
+                //       .map((d) => Image.asset(
+                //             d,
+                //             fit: BoxFit.cover,
+                //           ))
+                //       .toList()
+                // ],
+                // options: CarouselOptions(
+                //   // height: 85,
+                //   // aspectRatio: 2 / 3,
+                //   viewportFraction: 1,
+                //   initialPage: 0,
+                //   enableInfiniteScroll: false,
+                //   reverse: false,
+                //   autoPlay: false,
+                //   enlargeCenterPage: false,
+                //   // onPageChanged: (int index, CarouselPageChangedReason reason) =>
+                //   //     projects.setSelectedProject(index),
+                //   scrollDirection: Axis.horizontal,
+                // ),
               ),
             ),
-          ),
+          ],
         ),
       ),
     );
